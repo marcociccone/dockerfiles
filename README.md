cecb82c86930a858528dd3b78c741d0a36a089c9
marcociccone <marco.ciccone@polimi.it>
2021-04-19 14:55:18 +0000

    Cuda 11.1 ubuntu 20.04 dockerfiles

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dockerfiles
+
+Just run the build.sh file inside each directory.
+
+Compiled docker images can be downloaded from `https://hub.docker.com/r/airlab404/dl/`
---
+++ ubuntu20.04_cuda11.1_pytorch-1.8.1_native_y38/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# IMAGE_NAME="${USER}/dl:cuda10.1_pytorch-1.4_tf-1.15_py36"
+IMAGE_NAME="${USER}/dl:cuda11.1_pytorch-1.8.1_native_py38"
+echo "Building Dockerfile with image name: ${IMAGE_NAME}"
+docker build -t ${IMAGE_NAME} .
